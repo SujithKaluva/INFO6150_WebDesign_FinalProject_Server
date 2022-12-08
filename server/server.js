55b9b229a31fbc978b6e80f5e6ee7d449f5ad3eb
MUKESH\mukes <govardhanan.m@northeastern.edu>
2022-12-07 19:38:22 -0500

    Added dependencies for server file

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
-// Add Code
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+const env = require("dotenv");
+const mongoose = require("mongoose");
