ee9af546d80c4f654f86f6a601d1da0acbbe50b7
MUKESH\mukes <govardhanan.m@northeastern.edu>
2022-12-07 22:05:06 -0500

    Added comments model

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -1,1 +1,12 @@
-// TODO
+const mongoose = require("mongoose");
+
+const CommentsSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("newComment", CommentsSchema);
