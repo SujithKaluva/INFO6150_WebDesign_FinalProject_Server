a5fb1ed3bcd6d0fce6023ba5485749ee72643111
Sujith Kaluva <sujithkaluva1999@gmail.com>
2022-12-09 14:32:48 -0500

    Bug Fixes

--- server/connection.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// mongoose.connect(
-//   `mongodb+srv://mukesh:mukesh@cluster0.6z895zk.mongodb.net/chatAppMern?retryWrites=true&w=majority`,
-//   () => {
-//     console.log("connected to mongodb");
-//   }
-// );
