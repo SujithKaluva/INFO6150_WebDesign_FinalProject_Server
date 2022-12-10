8c834ec63ab78162341a44eb88ecca8c54e06470
Sujith Kaluva <sujithkaluva1999@gmail.com>
2022-12-09 21:52:17 -0500

    Bug Fixes 2

--- server/api/app.js
+++ server/api/app.js
@@ -1,24 +1,19 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import routes from "./routes/index.js";
-//import model from './models/index.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import routes from './routes/index.js';
 
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://fa-admin:flexyandiknowit@fa-cluster.ts2p7.mongodb.net/flexAppeal?retryWrites=true&w=majority",
-  () => {
-    console.log("connected to mongoDB");
-  }
-);
+
+mongoose.connect("mongodb+srv://mukesh:mukesh@cluster0.6z895zk.mongodb.net/flexAppeal?retryWrites=true&w=majority", ()=>{
+    console.log('connected to mongoDB');
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-/**
- * very important step
- * we are enrouting our requests to routes.index.js file here
- */
 
 routes(app);
 
